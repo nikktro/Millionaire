0c433da6dc2d7efa0b84159c97d5eff24bbd7cbe
Nikolay Trofimov <nikktro@gmail.com>
2020-09-01 23:29:18 +0300

    Initial Commit

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Nikolay Trofimov on 01.09.2020.
+//  Copyright © 2020 Nikolay Trofimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
